a8095efb074466dc5e58b6ecd171a56a4f7332ab
WILLIAM ALEXANDER MORENO PRIETO <williammoreno@javeriana.edu.co>
2019-09-28 15:06:12 -0500

    routing spalsh to main

--- app/src/main/java/com/example/silver_hacktrix/SplashScreen.java
+++ app/src/main/java/com/example/silver_hacktrix/SplashScreen.java
@@ -18,7 +18,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashScreen.this, CrearPublicacion.class);
+                Intent mainIntent = new Intent(SplashScreen.this, PrincipalActivity.class);
                 SplashScreen.this.startActivity(mainIntent);
                 SplashScreen.this.finish();
             }
