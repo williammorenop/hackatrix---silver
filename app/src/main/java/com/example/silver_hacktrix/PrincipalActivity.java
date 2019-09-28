2d087f04119014586e3df71812425778bb625ce3
WILLIAM ALEXANDER MORENO PRIETO <williammoreno@javeriana.edu.co>
2019-09-28 15:21:22 -0500

    routing principal to pool and login

--- app/src/main/java/com/example/silver_hacktrix/PrincipalActivity.java
+++ app/src/main/java/com/example/silver_hacktrix/PrincipalActivity.java
@@ -2,13 +2,36 @@ package com.example.silver_hacktrix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PrincipalActivity extends AppCompatActivity {
-
+    ImageButton imageButton;
+    ImageButton imageButton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        imageButton = findViewById(R.id.imageButton);
+        imageButton2 = findViewById(R.id.imageButton2);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent persona = new Intent(PrincipalActivity.this, PoolActivity.class);
+                startActivity(persona);
+            }
+        });
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ong = new Intent(PrincipalActivity.this, LogInActivity.class);
+                startActivity(ong);
+            }
+        });
     }
 }
